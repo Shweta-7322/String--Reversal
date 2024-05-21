c7711981ddb34d4ae8bd90630983e51c93112d52
Shweta  Walunj <142491674+Shweta-7322@users.noreply.github.com>
2024-05-21 16:33:47 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,9 @@
+def reverse_string(s):
+    return s[::-1]
+
+input_string = "Hello"
+
+# Reversing the string using the function
+reversed_string = reverse_string(input_string)
+
+print("Reversed string:", reversed_string)
